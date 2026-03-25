3e7a0e5c1aaf9de9e25260c5e604853caa2eb447
Justin <justinjones38@myyahoo.com>
2026-03-25 14:30:22 -0500

    Fixed navbar menu by using transform prop to open navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,25 @@ import './App.css'
 
 export default function App() {
   // Gets the width of the screensize
-  const [eventList, setEventList] = useState([]);
+  const [eventList, setEventList] = useState({});
+  const [error, setError] = useState(true);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const eventRes = await fetch(`https://api.jolpi.ca/ergast/f1/2026/races/?format=json`);
+        if (!eventRes.ok) {
+          throw new Error();
+        }
+        const eventData = await eventRes.json();
+        console.log(eventData.MRData);
+      } catch {
+
+      }
+
+    }
+    fetchData();
+  })
 
 
   return (
