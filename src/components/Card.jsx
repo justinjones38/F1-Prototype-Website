601cb937f3df093c29b848ea7a5083f47c206c5e
Justin <justinjones38@myyahoo.com>
2026-03-25 23:46:11 -0500

    Created Card Component to hold cards and DriverStandings Component to hold driver standings

---
+++ src/components/Card.jsx
@@ -0,0 +1,6 @@
+
+
+export default function Card(props) {
+    console.log(props);
+    return <h1>time</h1>
+}
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,13 +1,35 @@
 import { useState } from "react";
 import "./HeroSection.css"
+import Card from "./Card.jsx"
 
 export default function HeroSection({ calendar, driverStandings, constructorStandings }) {
-    console.log(driverStandings);
+
+    // Finding the next event, if no event, then section skipped
+    const findNextEvent = calendar.Races.find(event => parseInt(event.round) === parseInt(constructorStandings.round) + 1);
+    console.log(findNextEvent);
+
+    // Getting only the top 5 in driver standings
+    const filterDriverStandings = driverStandings.StandingsLists[0].DriverStandings.filter((driver, index) => (
+        index < 5
+    ));
+
+    console.log(calendar)
     return (
         <section className="hero-section">
             <h3 className="hero-section-title">
                 {calendar.season} Season - Round {constructorStandings.round} of {calendar.Races.length}
             </h3>
+
+            {findNextEvent ? 
+                <Card 
+                    raceInfo="Next Race"
+                    raceTitle={findNextEvent.raceName}
+                    circuitName={findNextEvent.Circuit.circuitName}
+                    date={findNextEvent.date}  
+                /> 
+                
+                : null}
+
             <div className="hero-section-standings">
                 <div className="hero-section-drivers">
 
