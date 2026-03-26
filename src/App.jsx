3c0c85a78c3c6179ef8259e18103ea8871e30289
Justin <justinjones38@myyahoo.com>
2026-03-25 20:58:07 -0500

    Added fetch to App.js to get calendar

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,46 @@ import { useEffect, useState } from 'react'
 import Navbar from './components/Navbar';
 
 import './App.css'
+import HeroSection from './components/HeroSection';
 
 export default function App() {
   // Gets the width of the screensize
-  const [eventList, setEventList] = useState({});
+  const [data, setData] = useState({})
   const [error, setError] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const eventRes = await fetch(`https://api.jolpi.ca/ergast/f1/2026/races/?format=json`);
-        if (!eventRes.ok) {
-          throw new Error();
-        }
-        const eventData = await eventRes.json();
-        console.log(eventData.MRData);
-      } catch {
+      const dataRes = await Promise.all([
+        fetch(`https://api.jolpi.ca/ergast/f1/2026/races/?format=json`),
+      ])
 
-      }
+      dataRes.forEach(res => {
+        if (!res.ok) {
+          throw new Error;
+        }
+      })
+
+      const fetchingData = await Promise.all(dataRes.map(res => res.json()));
+      const [calendar] = fetchingData;
+      setData({
+        calendar
+      })
+
+
 
     }
-    fetchData();
-  })
+    fetchData()
+  }, [])
 
+  if(!data || Object.keys(data).length === 0) {
+    return;
+  }
 
   return (
     <div className='app'>
       <header>
         <Navbar />
+        <HeroSection />
       </header>
     </div>
   )
