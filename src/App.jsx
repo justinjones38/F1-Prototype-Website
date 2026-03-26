c18eb1ae2699f804c5a53af95ceee97f58388349
Justin <justinjones38@myyahoo.com>
2026-03-25 22:41:42 -0500

    Added fetch for other data points

--- src/App.jsx
+++ src/App.jsx
@@ -7,42 +7,75 @@ import HeroSection from './components/HeroSection';
 export default function App() {
   // Gets the width of the screensize
   const [data, setData] = useState({})
-  const [error, setError] = useState(true);
+  const [error, setError] = useState(false);
+
+  const today = new Date();
+  const year = today.getFullYear();
+
+
 
   useEffect(() => {
     const fetchData = async () => {
-      const dataRes = await Promise.all([
-        fetch(`https://api.jolpi.ca/ergast/f1/2026/races/?format=json`),
-      ])
+      try {
+        const dataRes = await Promise.all([
+          fetch(`https://api.jolpi.ca/ergast/f1/${year}/races/?format=json`),
+          fetch(`https://api.jolpi.ca/ergast/f1/${year}/driverstandings/?format=json`),
+          fetch(`https://api.jolpi.ca/ergast/f1/${year}/constructors/?format=json`),
+          fetch(`https://api.jolpi.ca/ergast/f1/${year}/drivers/?format=json`),
+          fetch(`https://api.jolpi.ca/ergast/f1/${year}/constructors/?format=json`)
+        ])
 
-      dataRes.forEach(res => {
-        if (!res.ok) {
-          throw new Error;
-        }
-      })
+        dataRes.forEach(res => {
+          if (!res.ok) {
+            throw new Error;
+          }
+        })
 
-      const fetchingData = await Promise.all(dataRes.map(res => res.json()));
-      const [calendar] = fetchingData;
-      setData({
-        calendar
-      })
+        const fetchingData = await Promise.all(dataRes.map(res => res.json()));
 
+      //  Getting the values from the fetchingData Array
+        const [calendar, driverStandings, constructorStandings, drivers, constructors] = fetchingData;
 
-
+        setData({
+          calendar: calendar.MRData.RaceTable,
+          driverStandings: driverStandings.MRData.StandingsTable,
+          constructorStandings: constructorStandings.MRData.ConstructorTable,
+          drivers: drivers.MRData.DriverTable,
+          constructors: constructors.MRData.ConstructorTable
+        })
+      } catch {
+        setError(true);
+      }
     }
+
     fetchData()
   }, [])
 
-  if(!data || Object.keys(data).length === 0) {
+  if (!data || Object.keys(data).length === 0) {
     return;
   }
+
+
+  if (error) {
+    return;
+  }
+  console.log(data);
 
   return (
     <div className='app'>
       <header>
         <Navbar />
-        <HeroSection />
       </header>
+      <main>
+        <HeroSection
+          calendar={data.calendar.MRData}
+          driverStandings={data.driverStandings.MRData}
+          constructorStandings
+        />
+      </main>
+
+
+
     </div>
   )
 }
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import "./HeroSection.css"
 
 export default function HeroSection(props) {
-
-
     return (
         <h1>Test</h1>
     )
